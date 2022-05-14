b08c7c6ebbcef7d28b3764ed6309cb9e3feac3ea
Francesco <fduffyboscagli@gmail.com>
2022-05-14 14:48:36 -0700

    :art: formatted

--- src/scripts/script.js
+++ src/scripts/script.js
@@ -17,55 +17,52 @@ document.addEventListener("scroll", () => {
   }
 });
 
-
-
 //Parallax & Background Color
-const sections = document.querySelectorAll("section")
-const bodyTag = document.querySelector("body")
+const sections = document.querySelectorAll("section");
+const bodyTag = document.querySelector("body");
 
 const addMovement = () => {
   //get top and then middle of window
-  const topViewport = window.pageYOffset
-  const midViewport = topViewport + (window.innerHeight * 0.5)
+  const topViewport = window.pageYOffset;
+  const midViewport = topViewport + window.innerHeight * 0.5;
 
   sections.forEach((section, index) => {
-  
     //get middle of each section
-    const topSection = section.offsetTop
-    const midSection = topSection + (section.offsetHeight * 0.5)
-    
-    //get distance between section from the visible area of page
-    const distanceToSection = midViewport - midSection
-    
-    //picking tags to parallax
-    const contentTag = section.querySelector("div")
+    const topSection = section.offsetTop;
+    const midSection = topSection + section.offsetHeight * 0.5;
 
-    let distToSectionModifier = distanceToSection * 0.1
+    //get distance between section from the visible area of page
+    const distanceToSection = midViewport - midSection;
+
+    //picking tags to parallax
+    const contentTag = section.querySelector("div");
+
+    let distToSectionModifier = distanceToSection * 0.1;
     //for all the even sections (indices), make content move up instead of down
     //is the index divisible by 2
     //a.k.a. is the index's remainder 0? (modulo operator time: %)
     //this is also why distToSectionModifier is a let and not a const
-    if (index%2 == 0){
+    if (index % 2 == 0) {
       distToSectionModifier = distanceToSection * -0.1;
     }
 
     //apply parallax
-    contentTag.style.transform = `translateY(${distToSectionModifier}px)`
+    contentTag.style.transform = `translateY(${distToSectionModifier}px)`;
 
     //check the background
     if (distanceToSection > -300) {
-      const dataBackground = section.getAttribute("data-background")
-      bodyTag.style.backgroundColor = dataBackground
+      const dataBackground = section.getAttribute("data-background");
+      bodyTag.style.backgroundColor = dataBackground;
     }
   });
-}
+};
 
-addMovement()
+addMovement();
 
 document.addEventListener("scroll", () => {
-  addMovement()
-})
+  addMovement();
+});
 
-window.addEventListener("resize", () =>{
-  addMovement()
-}) 
+window.addEventListener("resize", () => {
+  addMovement();
+});
