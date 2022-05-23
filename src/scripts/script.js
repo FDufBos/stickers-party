83e15f4bf005b86b2b8787e1f44398511d8dba9a
Francesco <fduffyboscagli@gmail.com>
2022-05-22 17:05:10 -0700

    :bento: favicon

--- src/scripts/script.js
+++ src/scripts/script.js
@@ -3,3 +3,11 @@ import {} from "./stickers.js";
 import {} from "./uploadImage.js"
 
 
+const link = document.querySelector("link[rel~='icon']");
+if (!link) {
+    link = document.createElement('link');
+    link.rel = 'icon';
+    document.getElementsByTagName('head')[0].appendChild(link);
+}
+link.href = '"url:../assets/img/sparkle.ico';
+
